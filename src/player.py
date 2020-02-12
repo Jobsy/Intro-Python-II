4957caa409b4b4b4971e7e6be5ab08fd309820de
Oluwajoba <oluwajobabello@gmail.com>
2020-02-12 18:29:18 +0100

    add player class / player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,26 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+
+    def __init__(self, name, current_room, move):
+        self.name = name
+        self.current_room = current_room
+        self.move = move
+
+    def __str__(self):
+        output = ""
+        output += self.name + "\n"
+        output += self.current_room + "\n"
+        for m in self.move:
+            output += f"{m}\n"
+
+        output += f"Exit"
+
+        return output
+
+
+j = Player("Jobsy", "Room 1", ["n", "e", "s", "w"])
+
+print(j)
